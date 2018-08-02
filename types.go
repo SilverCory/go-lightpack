213700124a56e725bd9e230453e933be03f60fa8
Cory Redmond <ace@ac3-servers.eu>
2018-08-02 16:35:02 +0100

    Parse colours

--- types.go
+++ types.go
@@ -2,6 +2,7 @@ package lightpack
 
 import (
 	"errors"
+		"strconv"
 	"strings"
 )
 
@@ -45,4 +46,69 @@ func ParseStatusAPI(response string) (StatusAPI, error) {
 	}
 
 	return ret, nil
+}
+
+// I really hate spelling it wrong.
+type Color struct {
+	R, G, B int16
+}
+
+func ParseColors(response string) ([]Color, error) {
+	if strings.HasPrefix(response, "colors:") {
+		response = response[7:]
+	}
+
+	ledColors := strings.Split(response, ";")
+	ret := make([]Color, len(ledColors))
+
+	for _, v := range ledColors {
+		// Fixes trailing ; from making this fail.
+		if v == "" {
+			continue
+		}
+
+		led, color, err := ParseColor(v)
+		if err != nil {
+			return ret, err
+		}
+
+		ret[led] = color
+	}
+
+	return ret, nil
+}
+
+func ParseColor(color string) (int, Color, error) {
+	colorParts := strings.SplitN(color, "-", 2)
+	ledNumber, err := strconv.Atoi(colorParts[0])
+	if err != nil {
+		return ledNumber, Color{}, err
+	}
+
+	RGBVals := strings.Split(colorParts[1], ",")
+	if len(RGBVals) != 3 {
+		return ledNumber, Color{}, errors.New("invalid RGB values provided")
+	}
+
+	retColor := Color{}
+
+	if r, err := strconv.ParseInt(RGBVals[0], 10, 16); err != nil {
+		return ledNumber, Color{}, err
+	} else {
+		retColor.R = int16(r)
+	}
+
+	if g, err := strconv.ParseInt(RGBVals[1], 10, 16); err != nil {
+		return ledNumber, Color{}, err
+	} else {
+		retColor.G = int16(g)
+	}
+
+	if b, err := strconv.ParseInt(RGBVals[2], 10, 16); err != nil {
+		return ledNumber, Color{}, err
+	} else {
+		retColor.B = int16(b)
+	}
+
+	return ledNumber, retColor, nil
 }
--- types_test.go
+++ types_test.go
@@ -1,6 +1,9 @@
 package lightpack
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestParseStatus(t *testing.T) {
 
@@ -52,4 +55,13 @@ func TestParseStatusAPI(t *testing.T) {
 		t.Error("Parsing statusapi \"statusapi:invalid_input\" yeilds invalid response")
 	}
 
+}
+
+func TestParseColors(t *testing.T) {
+	input := `colors:0-97,102,103;1-102,104,105;2-102,104,105;3-101,103,102;4-97,100,103;5-100,100,100;6-102,103,103;7-98,101,103;8-94,96,98;9-92,94,95;10-94,96,98;11-93,95,96;12-93,94,95;13-92,93,92;14-90,93,93;15-90,93,94;16-90,93,93;17-90,93,93;18-89,92,90;19-90,92,92;20-93,94,95;21-93,94,94;22-93,93,93;23-90,93,93;24-90,93,93;25-89,92,92;26-94,96,97;27-96,97,96;28-90,93,94;29-94,96,97;`
+	if colors, err := ParseColors(input); err != nil {
+		t.Error(err)
+	} else {
+		fmt.Printf("%v\n", colors)
+	}
 }
